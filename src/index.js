2c645b298bd62003d381111951c50d8ef679fbd6
Abhishek Kumar Singh <krabhi1452@gmail.com>
2026-04-01 14:28:29 +0530

    index.js file setup

---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+
+const express = require("express");
+const app = express()
+const dotenv = require("dotenv")
+const cookieParser = require("cookie-parser")
+const cors = require("cors")
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3400
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:3001"
+];
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(
+    cors({
+    origin: function (origin, callback) {
+    // Allow requests not from origins  
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+
+    credentials: true,
+    })
+);
+
+// default raute
+app.get("/",(req, res) => {
+    return res.json({
+        success : true,
+        message : "Task manager server is running"
+    })
+})
+
+// app listing
+app.listen(PORT, ()=> {
+    console.log(`app is listning at port no ${PORT}`);
+})
